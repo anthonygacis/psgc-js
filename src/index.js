abc67c0a13acd86e34d831f55cd0eb1b5d51d0ec
Anthony Gacis (@ageesea) <anthony.gacis@yahoo.com>
2022-02-24 21:55:20 +0800

    Add getAll and getWithFilters

--- src/index.js
+++ src/index.js
@@ -1,14 +1,56 @@
 const fs = require('fs');
-let content = JSON.parse(fs.readFileSync('./src/extract/geo-data.json', 'utf8'));
+const { filter } = require('lodash');
 
-const getAll = () => content
-content.filter((item) => {
-    // if (item.income_classification) {
-    //     return item.income_classification.includes('1st')
-    // }
+const getAllRegions = () => {
+    let content = JSON.parse(fs.readFileSync(`./src/extract/geo-data.json`, 'utf8'));
+    return content.filter((item) => {
+        if (item.geographic_level) {
+            return item.geographic_level.includes('Reg')
+        }
+    })
+}
 
-    // return false;
-    return item.old_names
-})
+/**
+ * 
+ * @param {string} region region code
+ * @param {string} filters filter options
+ */
+const getWithFilters = (region, filters) => {
+    let content = JSON.parse(fs.readFileSync(`./src/extract/geo-reg-${parseInt(region)}.json`, 'utf8'));
+    let results = []
+    for (let [key, value] of Object.entries(content)) {
+        if (value.geographic_level == 'Reg') {
+            for (let [innerKey, innerValue] of Object.entries(value.sub_locations)) {
+                if (filters && filters.geographic_level && innerValue.geographic_level == filters.geographic_level) {
+                    delete innerValue.sub_locations
+                    results.push(innerValue)
+                } else {
+                    for (let [munKey, munValue] of Object.entries(innerValue.sub_locations)) {
+                        if (filters && filters.geographic_level && munValue.geographic_level == filters.geographic_level) {
+                            delete munValue.sub_locations
+                            results.push(munValue)
+                        } else {
+                            for (let [bgyKey, bgyValue] of Object.entries(munValue.sub_locations)) {
+                                if (filters && filters.geographic_level && bgyValue.geographic_level == filters.geographic_level) {
+                                    delete bgyValue.sub_locations
+                                    results.push(bgyValue)
+                                } else {
 
-module.exports = exports = { getAll }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return results
+}
+
+console.log(getWithFilters("05", {
+    geographic_level: 'Bgy'
+}))
+
+module.exports = exports = {
+    getWithFilters
+}
