1fdf26322b0e303690d345a9f4cac8e03ad83b14
Anthony Gacis (@ageesea) <anthony.gacis@yahoo.com>
2022-12-07 11:23:45 +0800

    Add under dev to doc

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'PSGC-JS',
-  tagline: 'A collection of philippine geographic data based on PSGC',
+  tagline: 'A collection of philippine geographic data based on PSGC (under construction)',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
