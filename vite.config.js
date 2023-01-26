fce593aa354d04f201e3b629983ea363a963adcb
Anthony S. Gacis (@ageesea) <anthony.gacis@yahoo.com>
2023-01-26 15:17:41 +0800

    Update data to 4Q (#28)

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import {resolve} from 'path'
+import {defineConfig} from 'vite'
 
 export default defineConfig({
     build: {
         lib: {
-            // Could also be a dictionary or array of multiple entry points
             entry: resolve(__dirname, 'src/index.js'),
             name: 'PSGC',
-            // the proper extensions will be added
             fileName: 'psgc-js'
         }
     },
